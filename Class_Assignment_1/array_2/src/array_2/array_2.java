5a38cf6924b96efb593cd4de1e06d6404c08d7ce
Owjasweeta <108125846+Owjasweeta@users.noreply.github.com>
2023-08-29 11:34:08 +0530

    Add files via upload

---
+++ Class_Assignment_1/array_2/src/array_2/array_2.java
@@ -0,0 +1,21 @@
+package array_2;
+
+public class array_2 {
+
+	public static void main(String[] args) {
+		int a[]= {2,8,6,10};
+		int i;
+		float total=0;
+		for(i=0; i<4; i++)
+		{
+			total=total+a[i];
+		}
+		System.out.println("Average:" + total/4);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Class_Assignment_1/array_2D/src/array_2D/array_2D.java
@@ -0,0 +1,19 @@
+package array_2D;
+
+public class array_2D {
+
+	public static void main(String[] args) {
+		int twod[][]= {
+				{30},{300,500},{3000,5000,5000}
+		};
+		System.out.println("The 2D array is:");
+		System.out.println(twod[0][0]);
+		System.out.println(twod[1][0]+" "+twod[1][1]);
+		System.out.println(twod[2][0]+" "+twod[2][1]+" "+twod[2][2]);
+		
+		
+		
+		
+	}
+
+}
